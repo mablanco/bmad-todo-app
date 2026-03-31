7aed3970035034275d9888e0cef87fc8d9e66a36
Marco Antonio Blanco <marcoantonio.blanco@protonmail.com>
2026-03-31 18:46:19 +0200

    feat: final epic 4 fixes

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -5,8 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
+    exclude: ['tests/e2e/**'],
+    include: ['src/**/*.{test,spec}.{ts,tsx}'],
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
   },
-});
+})
